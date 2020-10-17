84d31fa2f4b7e010fcc128920bded1e5ede1f79b
akashpundir931 <akashpundir931@gmail.com>
2020-10-17 22:04:00 +0530

    Responsive work start is in

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ var images = [];
 var time = 3000;
 
 images[0] = "img/1.png";
-images[1] = "img/slide1.png"
-images[2] = "img/2.png";
-images[3] = "img/slide3.png"
-images[4] = "img/3.png";
-images[5] = "img/4.png";
+images[1] = "img/2.png";
+images[2] = "img/3.png";
+images[3] = "img/4.png";
 
 function changeImg(){
     document.slide.src = images[i];
@@ -22,3 +20,35 @@ function changeImg(){
 }
 window.onload = changeImg;
 
+
+function clickcourses(){
+    var click1 = document.getElementById("course-item");
+    if(click1.style.display == "none"){
+        click1.style.display = "block";
+        click1.style.position = "absolute";
+        click1.style.padding = "10px";
+        click1.style.marginLeft="80px"
+        click1.style.boxShadow="2px 2px 4px #000000";
+        click1.style.borderRadius=" 0px 8px 0px 8px";
+        
+    }
+    else{
+        click1.style.display = "none";
+    }
+}
+
+function clickjobs(){
+    var click1 = document.getElementById("job-item");
+    if(click1.style.display == "none"){
+        click1.style.display = "block";
+        click1.style.position = "absolute";
+        click1.style.padding = "10px";
+        click1.style.marginLeft="80px"
+        click1.style.boxShadow="2px 2px 4px #000000";
+        click1.style.borderRadius=" 0px 8px 0px 8px";
+        
+    }
+    else{
+        click1.style.display = "none";
+    }
+}
